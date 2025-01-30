d438cae51641ea4d195d945893a1e966655b33f2
nickolss <nickolasmaiaraujo@gmail.com>
2025-01-30 11:02:28 -0300

    feat: Added a install script

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+# Installing Nix
+if ! command -v nix &>/dev/null; then
+  echo "Installing Nix..."
+  sh <(curl -L https://nixos.org/nix/install) --no-daemon
+else
+  echo "Nix is already installed."
+fi
+
+# Installing programs from Nix
+echo "Starting development setup..."
+sudo nixos-rebuild switch -I nixos-config=nix/configuration.nix
+
+# Configuring programs
+for script in config/*.sh; do
+  if [[ -f "$script" ]]; then
+    echo "Configuring $script"
+    bash "$script"
+  fi
+done
+
+echo "Finish him"
